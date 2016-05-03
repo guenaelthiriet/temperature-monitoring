01f00b2d35e10c3ba8f83ea2f5bfec1a05d63c14
Guenael Thiriet <gwenaelthiriet@gmail.com>
2016-05-03 19:12:16 -0400

    Change DB model

--- monitoring/models.py
+++ monitoring/models.py
@@ -8,17 +8,30 @@ class Humidity(models.Model):
     humity_level = models.FloatField()
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.humity_level
+
 
 class Temperature(models.Model):
     temperature = models.FloatField()
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.temperature
 
-class DataCollect(models.Model):
+class Location(models.Model):
+    location = models.CharField(max_length=50)
+    location_long_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Record(models.Model):
     humidity = models.ForeignKey(Humidity, on_delete=models.CASCADE)
     temperature = models.ForeignKey(Temperature, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField('date published')
 
-    location_text = models.CharField(max_length=200)
-    #
-    # choice_text = models.CharField(max_length=200)
-    # votes = models.IntegerField(default=0)
+    def __str__(self):
+        return 'Humidity: ' + self.humidity +\
+            'Temperature: ' + self.temperature+\
+            'Location: ' + self.location+\
+            'Date: ' + self.pub_date
